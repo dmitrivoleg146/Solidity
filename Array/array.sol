ecad1fa729e2974e74116235bcc2117e92005f67
Oleh <dmitrivoleg146@gmail.com>
2023-01-10 21:52:50 +0200

    array methods

--- Array/array.sol
+++ Array/array.sol
@@ -33,4 +33,35 @@ contract Array{
         }
     }
 
+    uint [] public arr;
+    uint [] public arr2 = [1,2,3];
+    uint [10] public fixedArray;
+
+    function get (uint i) public view returns (uint){
+        return arr[i];
+    }
+
+    function getArray() public view returns (uint [] memory){
+        return arr;
+    }
+    function push(uint i) public{
+        arr.push(i);
+    }
+    function pop() public {
+        arr.pop();
+    }
+    function getLength() public view returns (uint){
+        return arr.length;
+    }
+    function remove( uint i) public {
+        //delete does not change the length => it only reomve element
+        //and then those element by default = 0
+        delete arr[i];
+    }
+    function examples() external{
+        // create array in memory, only fixed size can be declared
+        uint[] memory a = new uint [] (5);
+    }
+    
+
 }
